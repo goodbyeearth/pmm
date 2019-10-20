f0f185761b559951168725c6f44b1200a116776b
goodbyeearth <312154160@qq.com>
2019-10-20 21:16:03 +0800

    删除 get_data.sh

--- get_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# 运行方法: ./get_data.sh [prefix_save_path] [data_episode]
-# 如：./get_data.sh ./dataset_test/expert_agent_ 200
-
-python run.py --generate_data --expert_path=${1} --data_episode=${2}
